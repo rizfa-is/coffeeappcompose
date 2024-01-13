1614164727048fd860031f8c62fcee235467de35
Rosyid Rosyadi <rosyid.rosyadi@privyid.tech>
2024-01-13 15:53:25 +0700

    Create category section

--- app/src/main/java/com/issog/coffeeappcompose/MainActivity.kt
+++ app/src/main/java/com/issog/coffeeappcompose/MainActivity.kt
@@ -4,17 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.issog.coffeeappcompose.model.dummyCategory
+import com.issog.coffeeappcompose.ui.component.CategoryItem
 import com.issog.coffeeappcompose.ui.component.Search
+import com.issog.coffeeappcompose.ui.component.SectionText
 import com.issog.coffeeappcompose.ui.theme.CoffeeappcomposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,6 +42,8 @@ class MainActivity : ComponentActivity() {
 fun CoffeeApp() {
     Column {
         Banner()
+        SectionText(title = stringResource(id = R.string.section_category))
+        CategoryRow()
     }
 }
 
@@ -48,6 +59,28 @@ fun Banner(modifier: Modifier = Modifier) {
         Search()
     }
 }
+
+@Composable
+fun CategoryRow(
+    modifier: Modifier = Modifier
+) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp)
+    ) {
+        items(dummyCategory, key = { it.textCategory }) {category ->
+            CategoryItem(category = category)
+        }
+    }
+}
+
+//@Preview(showBackground = true, device = Devices.PIXEL_4)
+//@Composable
+//fun CategoryRowPreview() {
+//    CoffeeappcomposeTheme {
+//        CategoryRow()
+//    }
+//}
 
 @Preview(showBackground = true, device = Devices.PIXEL_4)
 @Composable
